832f906f4010fdc7ddd88a8d174946de426ab2e4
Terin <Terin@Terin-HP>
2016-11-28 20:16:42 -0700

    Added AI to Computer player

--- src/connect/four/player/ComputerPlayer.java
+++ src/connect/four/player/ComputerPlayer.java
@@ -26,36 +26,73 @@ public class ComputerPlayer implements Player {
 	int m = board.getHeight();
         if (board.getMoveCount() == 0) {
             board.play((new Random()).nextInt(l), this);
-        } else {
-            Player opponent = getOpponent(board);
-            int maxMove = (new Random()).nextInt(l);
-            long maxScore = scoreMove(maxMove, m_depth, board, opponent);
-            long[] scores = new long[l];
-            for (int i = 0; i != l; ++i) {
-                if (board.whoPlayed(i, m-1) != null) continue;
-                long iScore = scoreMove(i, m_depth, board, opponent);
-                if (iScore > maxScore) {
-                    maxMove = i;
-                    maxScore = iScore;
-                }
-                scores[i] = iScore;
-            }
-	    while (board.whoPlayed(maxMove, m-1) != null) {
-                maxMove = (maxMove+1)%l;
-	    }
-            System.out.println(Arrays.toString(scores));
-            board.play(maxMove, this);
+        } 
+        
+        
+        else {
+        	
+        	System.out.println("Get Here");     	
+        	
+        	int pos = -1;
+        	Player self = getSelf(board);
+        	
+        	for( int i = 0 ; i < l ; i++){
+        		if (board.whoPlayed(i, m-1) != null) continue;
+        		Board temp = new Board(board);
+        		Board temp2 = new Board(board);        		
+        		temp.play(i, getOpponent(board));
+        		temp2.play(i, self);
+        		
+        		if(Game.detectWinner(temp, 4) != null){
+        			System.out.println("Check1 blocked" + i);
+        			pos = i;
+        		}
+        		
+        		if(Game.detectWinner(temp2, 4) != null){
+        			System.out.println("Check2 win" + i);
+        			pos = i;
+        		}
+        		
+        	}
+        	
+        	if(pos != -1){
+        		board.play(pos, this);
+        		System.out.println("Blocked or win move " + pos);
+        	}
+        	
+        	else{
+        	
+        		Player opponent = getOpponent(board);
+        		int maxMove = (new Random()).nextInt(l);
+        		long maxScore = scoreMove(maxMove, m_depth, board, opponent);
+        		long[] scores = new long[l];
+        		for (int i = 0; i != l; ++i) {
+        			if (board.whoPlayed(i, m-1) != null) continue;
+        			long iScore = scoreMove(i, m_depth, board, opponent);
+        			if (iScore > maxScore) {
+        				maxMove = i;
+        				maxScore = iScore;
+        			}
+        			scores[i] = iScore;
+        		}
+        		while (board.whoPlayed(maxMove, m-1) != null) {
+        			maxMove = (maxMove+1)%l;
+        		}
+        		System.out.println(Arrays.toString(scores));
+        		board.play(maxMove, this);
+        	}
         }
     }
 
     private long scoreMove(int x, int depth, ReadableBoard board, Player opponent) {
         int m = board.getHeight();
+        Player self = getSelf(board);
         if (board.whoPlayed(x, m-1) != null) return 0;
         Board myMove = new Board(board);
-        myMove.play(x, this);
+        myMove.play(x, self);
         int l = myMove.getWidth();
         long score = 0;
-        if (Game.detectWinner(myMove, 4) == this) {
+        if (Game.detectWinner(myMove, 4) == self) {
             score += Math.pow(l, depth);
         } else if (depth != 0) {
             for (int i = 0; i != l; ++i) {
@@ -80,12 +117,26 @@ public class ComputerPlayer implements Player {
         for (int i = 0; i != l; ++i) {
             for (int j = 0; j != m; ++j) {
                 Player here = board.whoPlayed(i, j);
-                if (here != null && here != this) {
+                if (here != null && !here.getName().equals("Computer")) {
                     return here;
                 }
             }
         }
         throw new Error("Can't call getOpponent on first turn.");
+    }
+    
+    private Player getSelf(ReadableBoard board) {
+        int l = board.getWidth();
+        int m = board.getHeight();
+        for (int i = 0; i != l; ++i) {
+            for (int j = 0; j != m; ++j) {
+                Player here = board.whoPlayed(i, j);
+                if (here != null && here.getName().equals("Computer")) {
+                    return here;
+                }
+            }
+        }
+        return this;
     }
 
 }
